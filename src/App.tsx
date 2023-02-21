d124f67694c7995be95c3e6d0dee903ff2258635
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-02-21 21:24:55 +0700

    A bit view Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import Home from "./Pages/Home";
 import Footer from "./Components/Footer";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen justify-between bg-slate-300">
-      <Home />
-      <Footer />
+        <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,6 +1,11 @@
+import { Link } from 'react-router-dom';
+
 const Home = () => {
     return ( 
-        <div className="text-2xl mx-auto mt-10">Halo</div>
+        <div className="mx-auto my-auto p-7 text-justify">
+            <h1 className="font-bold text-4xl text-center">Welcome</h1>
+            <p>This is A simple example of <Link to="https://reactjs.org/" target="_blank" className="hover:text-white">React</Link> + <Link to="https://tailwindcss.com/" target="_blank" className="hover:text-white">Tailwind</Link> + <Link to="https://www.typescriptlang.org/" target="_blank" className="hover:text-white">Typescript</Link></p>
+        </div>
      );
 }
  
